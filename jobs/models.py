e639015b5455f3c6f05d4e954d87978c43fa603e
Your Name <you@example.com>
2019-10-07 12:13:16 -0700

    added stuff in job's model file

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Job(models.Model): # object get saved into the database through models.Model?
+    image = models.ImageField(upload_to = 'images/') #tells where image will be saved
+    summary = models.CharField(max_length = 200)
+    
